70825416a96bb47b255bba88c8cb184877a82bb7
Sabeesh Velluvakkandy <sabeesh.velluvakkandy@centurylink.com>
2024-01-10 20:42:36 +0530

    Pod memory changes and otel jar changes

--- src/main/java/arp/training/cloudkitchen/repository/ProductRepository.java
+++ src/main/java/arp/training/cloudkitchen/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import arp.training.cloudkitchen.model.Product;
+import jakarta.persistence.QueryHint;
 
 @Repository
 public interface ProductRepository extends JpaRepository <Product, Long>  {
